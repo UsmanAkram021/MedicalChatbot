ea6456631fc91d5b879e0a016dda0973892c27c9
UsmanAkram021 <m.usman.akram021@gmail.com>
2024-10-16 21:26:55 +0500

    Get Started Project Structure Updated (1)

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Medical Chatbot",
+    version="0.0.1",
+    author="Muhammad Usman Akram",
+    author_email="m.usman.akram021@gmail.com",
+    install_requires=[],
+    packages=find_packages()
+)
