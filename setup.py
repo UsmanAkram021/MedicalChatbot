11cec394d95c89babaf5d1cdf5a0abe598e68cbf
UsmanAkram021 <m.usman.akram021@gmail.com>
2024-10-17 14:44:03 +0500

    Code Updated and Downloaded Embeddings Model (2)

--- setup.py
+++ setup.py
@@ -5,6 +5,15 @@ setup(
     version="0.0.1",
     author="Muhammad Usman Akram",
     author_email="m.usman.akram021@gmail.com",
-    install_requires=[],
+    install_requires=[
+        "ctransformers==0.2.25",
+        "sentence-transformers==3.1.1",
+        "langchain==0.3.0",
+        "langchain-community==0.3.0",
+        "Flask==3.0.3",
+        "pypdf==5.0.0",
+        "python-dotenv",
+        "pinecone"
+    ],
     packages=find_packages()
 )
